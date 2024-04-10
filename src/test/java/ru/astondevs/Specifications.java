0d299048d7e2b6c26c43ca061d3662a5e674a4de
Stas254 <Stas254@yandex.ru>
2024-04-10 23:50:01 +0300

    1. Создал ветку Lesson_16; 2. Написал API тесты, согласно задания №16; 3. C&P;

---
+++ src/test/java/ru/astondevs/Specifications.java
@@ -0,0 +1,55 @@
+package ru.astondevs;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+    public static RequestSpecification requestJsonSpec(String url) {
+        return new RequestSpecBuilder()
+                .setBaseUri(url)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static RequestSpecification requestTextSpec(String url) {
+        return new RequestSpecBuilder()
+                .setBaseUri(url)
+                .setContentType(ContentType.TEXT)
+                .build();
+    }
+
+    public static RequestSpecification requestFormSpec(String url) {
+        return new RequestSpecBuilder()
+                .setBaseUri(url)
+                .setContentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .build();
+    }
+
+    public static ResponseSpecification responseSpecOK200() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+
+    public static ResponseSpecification responseSpecERROR404() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(404)
+                .build();
+    }
+
+    public static ResponseSpecification responseSpecERROR400() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(400)
+                .build();
+    }
+
+    public static void installSpecification(RequestSpecification request, ResponseSpecification response) {
+        RestAssured.requestSpecification = request;
+        RestAssured.responseSpecification = response;
+
+    }
+}
